439bf1857fa28ca4579f55685e97058525ee9a0f
xutaotaotao <xutaotao1108@outlook.com>
2018-01-26 21:49:05 +0800

    add category-list but this style has a problrm that can't change style when click the list

--- client/js/home_page.js
+++ client/js/home_page.js
@@ -4,22 +4,56 @@
 window.onload = function () {
     axios.get('http://localhost:3000/movies')
         .then(function (response) {
-            let movies = response.data,str = ``;
+            let movies = response.data,moviesStr = ``;
+            let genres = [],categoryListStr = ``;
             movies.forEach(movie => {
                 let star = "★★★★★☆☆☆☆☆";
                 let rate = Math.round(movie.rating/2);
-                str += `<li class="movie-box">
+                moviesStr  += `<li class="movie-box">
                             <div class="movie-image">
                                 <img src="${movie.image}" alt="${movie.title}">
                             </div>
                             <div class="movie-title">${movie.title}</div>
                             <div class="movie-rating">${star.slice(5 - rate,10 - rate) + movie.rating}分</div>
-                        </li>`
+                        </li>`;
+
+                let movieGenres = movie.genres.split(",");
+                for(i in movieGenres){
+                    if(!genres.includes(movieGenres[i])){
+                        genres.push(movieGenres[i]);
+                    }
+                }
+            });
+            genres.forEach(genre => {
+                categoryListStr += `<label>${genre}</label>`;
             });
             let moviesRow = document.getElementById("moviesRow");
-            moviesRow.innerHTML += str;
+            moviesRow.innerHTML += moviesStr;
+
+            let movieCategory = document.getElementById("movieCategory");
+            movieCategory.innerHTML +=  categoryListStr;
+
+
         })
         .catch(function (errror) {
             console.log(errror);
         });
+
 };
+
+// 左侧分类列表的active样式控制
+function changeCategoryListStyle() {
+    let categoryList = $(".category-list label");
+    for( i in categoryList){
+        categoryList[i].onclick = function () {
+            for(x in categoryList){
+                categoryList[x].className="";
+                this.className = "list-active";
+            }
+        }
+    }
+}
+changeCategoryListStyle();
+
+
+
