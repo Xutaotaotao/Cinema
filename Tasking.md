f483409641675da5e4e331146ed45e51c54d6090
xutaotaotao <xutaotao1108@outlook.com>
2018-01-27 22:06:35 +0800

    add tasking.md and refacte the code

---
+++ Tasking.md
@@ -0,0 +1,10 @@
+
+*1.列出所给数据电影的所有分类*
+- function listAllCategoryList()
+- input：movies(array)
+- output:categoryList(array)
+
+*2.页面显示分类目录*
+- function showCategoryHtml(categoryList)
+- input:categoryList(array)
+- output:categoryListStr(string)
--- client/js/home_page.js
+++ client/js/home_page.js
@@ -1,31 +1,66 @@
 /**
  * Created by 徐涛焘 on 1/24/2018.
  */
+//1.列出所给数据电影的所有分类
+function listAllCategoryList(movies) {
+    let genres = [];
+    movies.forEach(movie => {
+        let movieGenres = movie.genres.split(",");
+        for(i in movieGenres){
+            if(!genres.includes(movieGenres[i])){
+                genres.push(movieGenres[i]);
+            }
+        }
+    });
+    return genres;
+}
+
+//2.页面显示分类目录
+function showCategoryHtml(categoryList) {
+    let categoryListStr = ``;
+    categoryList.forEach(category => {
+            categoryListStr += `<a>${category}</a>`;
+        }
+    );
+    let movieCategory = document.getElementById("movieCategory");
+    movieCategory.innerHTML +=  categoryListStr;
+
+}
+
+
+//2.左侧分类列表的active样式控
+//3.点击分类跳出相应的分类
+
 window.onload = function () {
     axios.get('http://localhost:3000/movies')
         .then(function (response) {
-            let movies = response.data,moviesStr = ``;
-            let genres = [],categoryListStr = ``;
-            movies.forEach(movie => {
-                let star = "★★★★★☆☆☆☆☆";
-                let rate = Math.round(movie.rating/2);
-                moviesStr  += `<li class="movie-box">
-                            <div class="movie-image">
-                                <img src="${movie.image}" alt="${movie.title}">
-                            </div>
-                            <div class="movie-title">${movie.title}</div>
-                            <div class="movie-rating">${star.slice(5 - rate,10 - rate) + movie.rating}分</div>
-                        </li>`;
+            let movies = response.data;
+            showCategoryHtml(listAllCategoryList(movies));
 
+            let moviesStr = ``;
+            let categoryListStr = ``;
+            movies.forEach(movie => {
                 let movieGenres = movie.genres.split(",");
                 for(i in movieGenres){
                     if(!genres.includes(movieGenres[i])){
                         genres.push(movieGenres[i]);
                     }
                 }
+
+/*                if(movieGenres.includes("家庭")){
+                    let star = "★★★★★☆☆☆☆☆";
+                    let rate = Math.round(movie.rating/2);
+                    moviesStr  += `<li class="movie-box">
+                            <div class="movie-image">
+                                <img src="${movie.image}" alt="${movie.title}">
+                            </div>
+                            <div class="movie-title">${movie.title}</div>
+                            <div class="movie-rating">${star.slice(5 - rate,10 - rate) + movie.rating}分</div>
+                        </li>`;
+                }*/
             });
             genres.forEach(genre => {
-                categoryListStr += `<label>${genre}</label>`;
+                categoryListStr += `<a>${genre}</a>`;
             });
             let moviesRow = document.getElementById("moviesRow");
             moviesRow.innerHTML += moviesStr;
@@ -41,19 +76,7 @@ window.onload = function () {
 
 };
 
-// 左侧分类列表的active样式控制
-function changeCategoryListStyle() {
-    let categoryList = $(".category-list label");
-    for( i in categoryList){
-        categoryList[i].onclick = function () {
-            for(x in categoryList){
-                categoryList[x].className="";
-                this.className = "list-active";
-            }
-        }
-    }
-}
-changeCategoryListStyle();
+
 
 
 
