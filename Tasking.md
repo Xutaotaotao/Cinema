9034e21443bdc6b929dc690efa04a718a4675af6
xutaotaotao <xutaotao1108@outlook.com>
2018-01-28 15:21:41 +0800

    task 1 has being done but when click the tags can't change the color

--- Tasking.md
+++ Tasking.md
@@ -1,10 +1,20 @@
 
-*1.列出所给数据电影的所有分类*
+*列出所给数据电影的所有分类*
 - function listAllCategoryList()
-- input：movies(array)
-- output:categoryList(array)
+- input：movies[array]
+- output:categoryList[array]
 
-*2.页面显示分类目录*
+*页面显示分类目录*
 - function showCategoryHtml(categoryList)
-- input:categoryList(array)
-- output:categoryListStr(string)
+- input:categoryList[array]
+- output:categoryListStr[string]
+
+*根据所给分类列出相应的电影*
+- function listCategoryMovies(category)
+- input:category[string],movies[array]
+- output:categoryMovies[array]
+
+*页面显示电影*
+- function showMoviesHtml(movies)
+- input:movies(array)
+- output:moviesStr[string]
--- client/js/home_page.js
+++ client/js/home_page.js
@@ -1,7 +1,7 @@
 /**
  * Created by 徐涛焘 on 1/24/2018.
  */
-//1.列出所给数据电影的所有分类
+//列出所给数据电影的所有分类
 function listAllCategoryList(movies) {
     let genres = [];
     movies.forEach(movie => {
@@ -15,7 +15,7 @@ function listAllCategoryList(movies) {
     return genres;
 }
 
-//2.页面显示分类目录
+//页面显示分类目录
 function showCategoryHtml(categoryList) {
     let categoryListStr = ``;
     categoryList.forEach(category => {
@@ -27,51 +27,53 @@ function showCategoryHtml(categoryList) {
 
 }
 
+//左侧分类列表的active样式控
+//根据所给分类列出相应的电影
+function listCategoryMovies(category,movies){
+    let categoryMovies = [];
+    movies.forEach(movie => {
+        let movieGenres = movie.genres.split(",");
+        if(movieGenres.includes(category)){
+            categoryMovies.push(movie);
+        }
+    });
+    return categoryMovies;
+}
 
-//2.左侧分类列表的active样式控
-//3.点击分类跳出相应的分类
-
-window.onload = function () {
-    axios.get('http://localhost:3000/movies')
-        .then(function (response) {
-            let movies = response.data;
-            showCategoryHtml(listAllCategoryList(movies));
-
-            let moviesStr = ``;
-            let categoryListStr = ``;
-            movies.forEach(movie => {
-                let movieGenres = movie.genres.split(",");
-                for(i in movieGenres){
-                    if(!genres.includes(movieGenres[i])){
-                        genres.push(movieGenres[i]);
-                    }
-                }
-
-/*                if(movieGenres.includes("家庭")){
-                    let star = "★★★★★☆☆☆☆☆";
-                    let rate = Math.round(movie.rating/2);
-                    moviesStr  += `<li class="movie-box">
+// 页面显示分类电影
+function showMoviesHtml(movies) {
+    let moviesStr = ``;
+    movies.forEach(movie => {
+        let star = "★★★★★☆☆☆☆☆";
+        let rate = Math.round(movie.rating/2);
+        moviesStr  += `<li class="movie-box" >
                             <div class="movie-image">
                                 <img src="${movie.image}" alt="${movie.title}">
                             </div>
                             <div class="movie-title">${movie.title}</div>
                             <div class="movie-rating">${star.slice(5 - rate,10 - rate) + movie.rating}分</div>
                         </li>`;
-                }*/
+    });
+    return moviesStr;
+}
+
+window.onload = function () {
+    axios.get('http://localhost:3000/movies')
+        .then(function (response) {
+            let movies = response.data;
+            showCategoryHtml(listAllCategoryList(movies));
+            $("#movieCategory").on("click","a",function (event) {
+                let moviesRow = document.getElementById("moviesRow");
+               if(moviesRow.innerText === ""){
+                   moviesRow.innerHTML += showMoviesHtml(listCategoryMovies($(this).text(),movies));
+               }else{
+                   $("#moviesRow").html("");
+                   moviesRow.innerHTML += showMoviesHtml(listCategoryMovies($(this).text(),movies));
+               }
             });
-            genres.forEach(genre => {
-                categoryListStr += `<a>${genre}</a>`;
-            });
-            let moviesRow = document.getElementById("moviesRow");
-            moviesRow.innerHTML += moviesStr;
-
-            let movieCategory = document.getElementById("movieCategory");
-            movieCategory.innerHTML +=  categoryListStr;
-
-
         })
-        .catch(function (errror) {
-            console.log(errror);
+        .catch(function (error) {
+            console.log(error);
         });
 
 };
